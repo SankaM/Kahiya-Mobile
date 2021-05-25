26bd229a3f148ff5f2a351b424aeac9d1a05da3a
desdrianton <desdrianton@gmail.com>
2021-05-26 04:31:27 +0700

    [ePatient] Notification page

---
+++ ePatient/10__code/monda_epatient/lib/_4__presentation/page/_2__notification/widget__notification_item.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/components/avatar/gf_avatar.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:monda_epatient/_0__infra/asset.dart';
+import 'package:monda_epatient/_0__infra/screen_util.dart';
+import 'package:monda_epatient/_0__infra/style.dart';
+
+class NotificationItem extends StatelessWidget {
+  final String doctorName;
+
+  final String status;
+
+  final String dateTime;
+
+  final String fuzzyDateTime;
+
+  final String doctorImage;
+
+  NotificationItem({required this.doctorName, required this.status, required this.dateTime, required this.fuzzyDateTime, required this.doctorImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: ScreenUtil.heightInPercent(3), bottom: ScreenUtil.heightInPercent(2)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(doctorName, style: Style.defaultTextStyle(color: Colors.black, fontWeight: FontWeight.w600),),
+                SizedBox(height: ScreenUtil.heightInPercent(1),),
+                Text(status, style: Style.defaultTextStyle(color: Colors.black),),
+                SizedBox(height: ScreenUtil.heightInPercent(0.75),),
+                Text(dateTime, style: Style.defaultTextStyle(color: Colors.grey, fontSize: Style.fontSize_S),),
+              ]
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(fuzzyDateTime, style: Style.defaultTextStyle(color: Colors.grey, fontSize: Style.fontSize_S, fontWeight: FontWeight.w500),),
+                  SizedBox(height: ScreenUtil.heightInPercent(1),),
+                  Container(
+                    child: GFAvatar(
+                      backgroundImage: AssetImage(doctorImage),
+                      shape: GFAvatarShape.square,
+                      size: ScreenUtil.heightInPercent(3),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                ]
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- ePatient/10__code/monda_epatient/lib/_4__presentation/page/_2__notification/widget__notification_tabs.dart
+++ ePatient/10__code/monda_epatient/lib/_4__presentation/page/_2__notification/widget__notification_tabs.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:monda_epatient/_0__infra/asset.dart';
 import 'package:monda_epatient/_0__infra/screen_util.dart';
 import 'package:monda_epatient/_0__infra/style.dart';
+import 'package:monda_epatient/_4__presentation/page/_2__notification/widget__notification_item.dart';
 import 'package:monda_epatient/_9__modify/bubble_tab_indicator__0_1_6/bubble_tab_indicator.dart';
 import 'package:monda_epatient/_9__modify/flutter/custom_tabs.dart';
 
@@ -32,11 +34,11 @@ class NotificationTabs extends StatelessWidget {
               ),
             ),
             Container(
-              height: 120,
+              height: ScreenUtil.heightInPercent(75),
               child: CustomTabBarView(
                 children: <Widget>[
-                  Container(child: Center(child: Text('Notifications'),),),
-                  Container(child: Center(child: Text('Medication Alerts'),),),
+                  _NotificationTab(),
+                  _MedicationAlertTab(),
                 ],
               ),
             ),
@@ -44,5 +46,57 @@ class NotificationTabs extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class _NotificationTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.red,
+      height: ScreenUtil.heightInPercent(80),
+      child: ListView(
+        children: [
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          NotificationItem(doctorName: 'Dr. Melinda Margot', status: 'Appointment Confirmed', dateTime: 'Mon | 17 May, 2021 | 12:00 PM', fuzzyDateTime: 'Just Now', doctorImage: Asset.png_face01,),
+          Divider(),
+          SizedBox(height: ScreenUtil.heightInPercent(20),),
+        ],
+      ),
+    );
+  }
+}
+
+class _MedicationAlertTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text('Medication Alerts'),),);
   }
 }
